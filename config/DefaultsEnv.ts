9b8fa158b1daa7eb4a9bd2a411f6fa0612f91fac
Denis Smorodin <smodean73@gmail.com>
2020-04-12 23:17:13 +0400

    refactoring

---
+++ config/DefaultsEnv.ts
@@ -0,0 +1,9 @@
+declare abstract class DefaultsEnv {
+  public readonly DEV_SERVER_PORT: number;
+
+  public readonly PROXY_TARGET?: string;
+
+  public readonly OPEN_BROWSER: boolean;
+}
+
+export default DefaultsEnv;
--- config/Env.ts
+++ config/Env.ts
@@ -1,16 +1,9 @@
 import { config, DotenvParseOutput } from 'dotenv';
 
 import paths from './paths';
+import DefaultsEnv from './DefaultsEnv';
 
-function getMessage(message: string): string {
-  const first = Array(message.length + 12).fill('*').join('');
-  const middle = `***   ${message}   ***`;
-  const last = first;
-
-  return `\n\t${first}\n\t${middle}\n\t${last}\n`;
-}
-
-export default class Env {
+export default class Env implements DefaultsEnv {
   private static getConfig(path: string): DotenvParseOutput {
     const { error, parsed } = config({ path });
 
@@ -23,43 +16,66 @@ export default class Env {
     return parsed;
   }
 
-  public readonly IS_PRODUCTION: boolean;
+  private static getMessage(message: string): string {
+    const first = Array(message.length + 12).fill('*').join('');
+    const middle = `***   ${message}   ***`;
+    const last = first;
 
-  public readonly IS_DEVELOPMENT: boolean;
+    return `\n\t${first}\n\t${middle}\n\t${last}\n`;
+  }
 
-  public readonly DEV_SERVER_PORT: number;
+  public readonly IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
-  public readonly PROXY_TARGET?: string;
+  public readonly IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
-  public readonly OPEN_BROWSER: boolean;
+  public readonly DEV_SERVER_PORT: DefaultsEnv['DEV_SERVER_PORT'];
 
-  constructor(defaults: Pick<Env, 'PROXY_TARGET' | 'OPEN_BROWSER' | 'DEV_SERVER_PORT'>) {
-    this.IS_PRODUCTION = process.env.NODE_ENV === 'production';
-    this.IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+  public readonly PROXY_TARGET: DefaultsEnv['PROXY_TARGET'];
+
+  public readonly OPEN_BROWSER: DefaultsEnv['OPEN_BROWSER'];
+
+  private readonly defaults: DefaultsEnv;
+
+  constructor(defaults: DefaultsEnv) {
+    this.defaults = defaults;
 
     this.initConfig();
 
-    const { DEV_SERVER_PORT = `${defaults.DEV_SERVER_PORT}`, OPEN_BROWSER, PROXY_TARGET = defaults.PROXY_TARGET } = process.env;
+    this.DEV_SERVER_PORT = this.getDevServerPort();
 
-    this.DEV_SERVER_PORT = parseInt(DEV_SERVER_PORT, 10);
+    this.PROXY_TARGET = this.getProxyTarget();
 
-    this.PROXY_TARGET = PROXY_TARGET;
+    this.OPEN_BROWSER = this.getOpenBrowser();
+  }
 
-    this.OPEN_BROWSER = ((): boolean => {
-      if (OPEN_BROWSER === 'true' || OPEN_BROWSER === 'false') {
-        return JSON.parse(OPEN_BROWSER);
-      }
+  private getProxyTarget(): DefaultsEnv['PROXY_TARGET'] {
+    const { PROXY_TARGET = this.defaults.PROXY_TARGET } = process.env;
 
-      if (OPEN_BROWSER) {
-        return Boolean(OPEN_BROWSER);
-      }
+    return PROXY_TARGET;
+  }
 
-      return defaults.OPEN_BROWSER;
-    })();
+  private getDevServerPort(): DefaultsEnv['DEV_SERVER_PORT'] {
+    const { DEV_SERVER_PORT = `${this.defaults.DEV_SERVER_PORT}` } = process.env;
+
+    return parseInt(DEV_SERVER_PORT, 10);
+  }
+
+  private getOpenBrowser(): DefaultsEnv['OPEN_BROWSER'] {
+    const { OPEN_BROWSER } = process.env;
+
+    if (OPEN_BROWSER === 'true' || OPEN_BROWSER === 'false') {
+      return JSON.parse(OPEN_BROWSER);
+    }
+
+    if (OPEN_BROWSER) {
+      return Boolean(OPEN_BROWSER);
+    }
+
+    return this.defaults.OPEN_BROWSER;
   }
 
   private initConfig(): DotenvParseOutput {
-    const { getConfig } = Env;
+    const { getConfig, getMessage } = Env;
 
     try {
       if (this.IS_DEVELOPMENT) {
